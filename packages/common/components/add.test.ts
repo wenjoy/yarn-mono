b15f6588be1c263d1d9605b6c9e94ff95659a4ee
wenjoy <laojiaodaoniu@126.com>
2023-08-29 17:11:52 +0800

    chore: add jest for packages common

---
+++ packages/common/components/add.test.ts
@@ -0,0 +1,5 @@
+import { add } from './add'
+
+test('should add two number', () => {
+  expect(add(1,5)).toEqual(6)
+})
--- packages/common/jest.config.ts
+++ packages/common/jest.config.ts
@@ -4,6 +4,8 @@
  */
 
 export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
